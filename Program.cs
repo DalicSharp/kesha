812948c071708c1ee29dddef559a757b7a4f701e
Dalerjon Shokirov <dalershokirov10@gmail.com>
2024-10-07 00:52:44 +0500

    Dastlabki commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+string greeting = $"Hello, {name}";
+System.Console.WriteLine(greeting);
+
+System.Console.Write("Enter your age: ");
+string priceAsString = Console.ReadLine();
+System.Console.WriteLine("Converting... ");
+int age = Convert.ToInt32(priceAsString);
+System.Console.WriteLine($"Successfully converted! {age}");
+int keschaAge = 3;
+int ageDifference = age - keschaAge;
+System.Console.WriteLine($"The difference between your and Kescha's age is {ageDifference}");
